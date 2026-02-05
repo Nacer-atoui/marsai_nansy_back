f9f6747e33dc6eeaf4d3085bb1c02752f333e68e
yonna-laplateforme <yonna.merlini@laplateforme.io>
2026-02-05 10:14:50 +0100

    mise a jour back

--- src/server.ts
+++ src/server.ts
@@ -22,11 +22,11 @@ app.use(express.json());
 
 // Connexion à la Base de Données
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'mars_ia',
-    port: Number(process.env.DB_PORT) || 8889
+    host: process.env.DB_HOST ?? '127.0.0.1',
+    user: process.env.DB_USER ?? 'root',
+    password: process.env.DB_PASSWORD ?? 'root',
+    database: process.env.DB_NAME ?? 'mars_ia',
+    port: Number(process.env.DB_PORT) ?? 8889
 });
 
 db.connect((err) => {
