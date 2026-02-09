11695c84303af22e159d36fe76eb62a4efd9a4cb
yonna-laplateforme <yonna.merlini@laplateforme.io>
2026-02-09 15:37:21 +0100

    modifback

--- src/routes/site.routes.ts
+++ src/routes/site.routes.ts
@@ -1,22 +1,12 @@
-import express from 'express';
-import mysql from 'mysql2';
-const router = express.Router();
+import { Router } from 'express';
+import { syncTranslations, getTranslations } from '../controllers/translationController';
 
-// Connexion BDD
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
-});
+const router = Router();
 
-// Route pour récupérer les textes de la Home Page
-router.get('/home-config', (req, res) => {
-    db.query('SELECT * FROM site_config WHERE id = 1', (err, results: any) => {
-        if (err) return res.status(500).json(err);
-        res.json(results[0]); // On renvoie l'objet config
-    });
-});
+// Route pour l'admin (POST)
+router.post('/sync', syncTranslations);
+
+// Route pour i18next (GET) - C'est celle-ci que le Front va appeler
+router.get('/translations/:lang', getTranslations);
 
 export default router;
